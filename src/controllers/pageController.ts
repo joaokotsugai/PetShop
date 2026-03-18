7b45fc301077f0e03d0bd83834cd1609aa99335b
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-03-18 08:25:18 -0300

    ajustando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,23 +1,42 @@
 import { Request, Response } from "express";
-import { title } from "process";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
     banner: {
         title: 'Todos os animais',
         background: 'allanimals.jpg'
-        }
+        },
+    menu: createMenuObject('all')
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+    banner: {
+        title: 'Cachorros',
+        background: 'banner_dog.jpg'
+        },
+    menu: createMenuObject('dogs')
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+    banner: {
+        title: 'Gatos',
+        background: 'banner_cat.jpg'
+        },
+    menu: createMenuObject('cats')
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
-    res.render('pages/page');
+    res.render('pages/page', {
+    banner: {
+        title: 'Peixes',
+        background: 'banner_fish.jpg'
+        },
+    menu: createMenuObject('fishes')
+    });
 }
